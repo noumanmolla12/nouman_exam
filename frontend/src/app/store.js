8a684d7a836ca3537883be07c1d1b06f938fcc8e
Mohammad Nouman MOLLA <101586016+noumanmolla12@users.noreply.github.com>
2025-07-30 00:12:50 +0530

    Update store.js

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,26 +1,32 @@
-// src/app/store.js
 
 import { configureStore } from '@reduxjs/toolkit';
-import authadminSlice from '../features/authadminSlice';
-import adminSlice from '../features/adminSlice'; 
+ import authadminSlice from '../features/authadminSlice';
+ import adminSlice from '../features/adminSlice';
 import studentSlice from '../features/studentSlice';
 import categorySlice from '../features/categorySlice';
 import topicSlice from '../features/topicSlice';
 import questionSlice from '../features/questionSlice';
 import answerSlice from '../features/answerSlice';
-import pathReducer from '../features/globalpaths';
+
+
+ import pathReducer from '../features/globalpaths';
+
+
 
 const Store = configureStore({
   reducer: {
     useradmin: authadminSlice,
-    admins: adminSlice,
+    //userstudent: authstudentSlice,
+     admins: adminSlice,
     students: studentSlice,
-    path: pathReducer,
-    categories: categorySlice,
-    topics: topicSlice,
-    questions: questionSlice,
-    answers: answerSlice,
+     path: pathReducer,
+     categories: categorySlice,
+     topics: topicSlice,
+     questions: questionSlice,
+     answers: answerSlice,
+    
   },
 });
+
 
 export default Store;
