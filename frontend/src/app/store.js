00a73b69ca05267facb0be5c82968be7c137b453
Mohammad Nouman Molla <noumanmolla12@gmail.com>
2025-07-31 17:33:43 +0530

    Replaced all frontend files

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,30 +1,36 @@
 
 import { configureStore } from '@reduxjs/toolkit';
- import authadminSlice from '../features/authadminSlice';
- import adminSlice from '../features/adminSlice';
+// import authadminSlice from '../features/authadminSlice';
+ import authstudentSlice from '../features/authstudentSlice'; 
+// import adminSlice from '../features/adminSlice';
 import studentSlice from '../features/studentSlice';
+
 import categorySlice from '../features/categorySlice';
 import topicSlice from '../features/topicSlice';
-import questionSlice from '../features/questionSlice';
+
 import answerSlice from '../features/answerSlice';
+import questionSlice from '../features/questionSlice';
 
+import examSlice from '../features/examSlice';
+import resultSlice from '../features/resultSlice';
 
- import pathReducer from '../features/globalpaths';
+// import pathReducer from '../features/globalpaths';
 
 
 
 const Store = configureStore({
   reducer: {
-    useradmin: authadminSlice,
-    //userstudent: authstudentSlice,
-     admins: adminSlice,
+    // useradmin: authadminSlice,
+    userstudent: authstudentSlice,
+    // admins: adminSlice,
     students: studentSlice,
-     path: pathReducer,
-     categories: categorySlice,
-     topics: topicSlice,
-     questions: questionSlice,
-     answers: answerSlice,
-    
+    // path: pathReducer,
+    categories: categorySlice,
+    topics: topicSlice,
+    questions: questionSlice,
+    answers: answerSlice,
+    exam: examSlice,
+    result: resultSlice,
   },
 });
 
